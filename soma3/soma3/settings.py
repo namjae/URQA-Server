a4420c196249774a8ffbea98864dc92b1c92a5c8
YangHyunchul <wolfses@hotmail.com>
2013-08-08 15:06:43 +0900

    경로설정 변경
    setting.py 상대경로로변경

--- soma3/soma3/settings.py
+++ soma3/soma3/settings.py
@@ -111,7 +111,7 @@ ROOT_URLCONF = 'soma3.urls'
 WSGI_APPLICATION = 'soma3.wsgi.application'
 
 TEMPLATE_DIRS = (
-    os.path.join(os.path.dirname(__file__), 'templates'),
+    os.path.join(os.path.dirname(__file__),os.path.join(os.path.pardir, 'templates')),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
