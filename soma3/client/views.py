b7d06156d8a8426456ae1156af016c37019daa79
YangHyunchul <wolfses@hotmail.com>
2013-08-09 17:39:12 +0900

    client url설

--- soma3/client/views.py
+++ soma3/client/views.py
@@ -1,10 +1,22 @@
 # Create your views here.
 
-import random
+import time
 import simplejson
 from django.http import HttpResponse
 
 def connect(request):
-    sessionID = random.randint(1,1000000)
+    sessionID = time.time()
     #return HttpResponse(sessionID)
     return HttpResponse(simplejson.dumps({'sessionID':sessionID}), 'application/json');
+
+def receive_exception(request):
+
+    return HttpResponse('test')
+
+def receive_uncaught(request):
+
+    return HttpResponse('test')
+
+def receive_eventpath(request):
+
+    return HttpResponse('test')
