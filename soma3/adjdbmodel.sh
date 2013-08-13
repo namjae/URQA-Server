1efc842cde651cfd055c9abbe6cbb21c7d8f6746
YangHyunchul <wolfses@hotmail.com>
2013-08-13 16:38:50 +0900

    model maker 수정

--- soma3/adjdbmodel.sh
+++ soma3/adjdbmodel.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 ./manage.py inspectdb > ./urqa/models.py
-./manage.py inspectdb > ./client/models.py
-
-
 sed -i 's/\.IntegerField(primary_key=True)/.AutoField(primary_key=True)/g' ./client/models.py
-sed -i 's/\.IntegerField(primary_key=True)/.AutoField(primary_key=True)/g' ./urqa/models.py
+
+cp ./urqa/models.py ./client/models.py
+cp ./urqa/models.py ./usermanage/models.py
