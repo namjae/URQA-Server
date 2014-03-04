df7e448a3b1ae391526db14327015c77d0d0d522
Yang Hyunchul <wolfses@hotmail.com>
2014-03-04 20:54:03 +0900

    하나빼먹음 다시 추가함

--- soma3/static/js/user.js
+++ soma3/static/js/user.js
@@ -1703,7 +1703,8 @@ $("head").styleReady(function(){
         }
         function DrawChart3(categories,data){
             $('.notHover').eq(3).empty().append($('<td id="decs"></td>'))
-            var colors = [ "#de6363", "#5a9ccc", "#72c380", "#cccdc7", "#9d61dd", "#6371dc", "#dca763", "#a96f6e", "#6fa79a", "#737270" ]
+            var colors = ["#fce700","#eda700","#ff6d05","#fb405a","#de3cad","#aa26aa","#211a74","#0046ae","#0086cf","#04ae8e",
+                          "#55a51a","#bbd004","#786a65","#cab280","#e7a1af","#e3abd7","#8584a3","#71a38a","#da88b8","#6492b6"]
             for(var i=0;i<data[0]['data'].length;i++)
                 data[0]['data'][i]={y:data[0]['data'][i],color:colors[i%colors.length]}
             chart3 = new Highcharts.Chart({
