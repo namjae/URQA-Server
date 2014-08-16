a62ed38b0b886203feb07bba62e0b019c5ca8c21
AndyHwang <hwanghakbeom@gmail.com>
2014-08-17 03:07:40 +0900

    test it

--- soma3/urqa/views.py
+++ soma3/urqa/views.py
@@ -17,8 +17,10 @@ from soma3.settings import STATIC_URL
 # from utility import getUTCawaredatetime
 
 # from oauth2client.client import OAuth2WebServerFlow
+ def index(request):
+    return render(request, 'index.html')
 
-def index(request):
+def login(request):
 
     #만약 로그인 되어 있다면!!
     if request.user.is_authenticated():
